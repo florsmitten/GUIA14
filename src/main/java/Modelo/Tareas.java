03d8b07914b32c5520f16ddc60529c47360b489d
florsmitten <schmidt.mariaf@gmail.com>
2020-11-28 13:51:33 -0300

    Initial commit

---
+++ src/main/java/Modelo/Tareas.java
@@ -0,0 +1,53 @@
+package Modelo;
+
+import java.util.Calendar;
+
+public class Tareas {
+
+    private boolean tareaRealizada;
+    private Calendar fechaFinTarea;
+    private String horaFinTarea;
+    private Operarios operario;
+
+    public Tareas(boolean tareaRealizada, Calendar fechaFinTarea, String horaFinTarea, Operarios operario) {
+        this.tareaRealizada = tareaRealizada;
+        this.fechaFinTarea = fechaFinTarea;
+        this.horaFinTarea = horaFinTarea;
+        this.operario = operario;
+    }
+
+    public Tareas() {
+    }
+
+    public boolean isTareaRealizada() {
+        return tareaRealizada;
+    }
+
+    public void setTareaRealizada(boolean tareaRealizada) {
+        this.tareaRealizada = tareaRealizada;
+    }
+
+    public Calendar getFechaFinTarea() {
+        return fechaFinTarea;
+    }
+
+    public void setFechaFinTarea(Calendar fechaFinTarea) {
+        this.fechaFinTarea = fechaFinTarea;
+    }
+
+    public String getHoraFinTarea() {
+        return horaFinTarea;
+    }
+
+    public void setHoraFinTarea(String horaFinTarea) {
+        this.horaFinTarea = horaFinTarea;
+    }
+
+    public Operarios getOperario() {
+        return operario;
+    }
+
+    public void setOperario(Operarios operario) {
+        this.operario = operario;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Flor
+  Date: 27/11/2020
+  Time: 23:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>HOLA</title>
+</head>
+<body>
+
+</body>
+</html>
